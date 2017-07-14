f600126ca4f8a13292ffc04c720730262db385a7
LunaticBlue <lunaticbleu@gmail.com>
2017-07-14 13:18:37 -0400

    Add files via upload
    update!

--- entity.js
+++ entity.js
@@ -1,26 +1,34 @@
 
 
 //Cape Generator
+var phIDs = 0
 abilityClasses = ["Mover", "Shaker", "Brute", "Breaker", "Master", "Tinker", "Blaster", "Thinker", "Striker", "Changer", "Trump", "Stranger", ]
 abilityPowermods = [1, 3, 5, 0, 5, 0, 5, 1, 7, 3, 0, 1,]  
 abilityHealthmods = [7, 3, 5, 0, 1, 0, 3, 1, 1, 5, 0, 7,]
 function cape(power, health, ability, age){
+	this.phID = 0
 	this.power = power;
 	this.health = health;
 	this.ability = ability;
 	this.age = age;
 	this.name = "Place Holder";
+	this.posted = false;
 	this.generate = function(){
 		this.power = Math.floor(Math.random() * 10) + 1;
 		this.health = Math.floor(Math.random() * 10) + 1;
-		abilityIndex = Math.floor(Math.random()*12);
-		this.ability = abilityClasses[abilityIndex];
-		this.power += abilityPowermods[abilityIndex];
-		this.health += abilityHealthmods[abilityIndex];
-		this.abilityIndex = abilityIndex;
+		this.abilityIndex = Math.floor(Math.random()*12);
+		this.ability = abilityClasses[this.abilityIndex];
+		this.power += abilityPowermods[this.abilityIndex];
+		this.health += abilityHealthmods[this.abilityIndex];
+		this.phID = phIDs;
+		phIDs += 1;
 	}
 	this.post = function(elem){
-		elem.innerHTML = "<li>Name: "+ this.name +"</li><li>, Class: "+ this.ability +"</li><li>, Power: "+ this.power +"</li><li>, Survivability: "+ this.health +"</li>"
+		elem.innerHTML += "<div class = 'capeContainer'id = 'ph" + this.phID + "'><ul><li>Name: "+ this.name +"</li><li>Class: "+ this.ability +"</li><li>Power: "+ this.power +"</li><li>Survivability: "+ this.health +"</li></ul><button onclick='killCape("+this.phID+")' type='button'>Terminate.</button></div>";
+		this.posted = true;
+	}
+	this.die = function(){
+		document.getElementById("ph"+ this.phID +"").innerHTML = "";
 	}
 }
 
@@ -59,6 +67,14 @@ var entity = {
 		this.race = str;
 		document.getElementById("race").innerHTML = "Race: " + this.race
 	},
+	capePurge: function(){
+		for (var i = this.capePopulation.length - 1; i >= 0; i--) {
+			if(this.capePopulation[i] == undefined){
+				this.capePopulation.splice(i,1);
+			}
+		}
+		this.capes = this.capePopulation.length;
+	},
 	
 }
 function addData(){
@@ -77,13 +93,7 @@ function addCapes(){
 	var capeHolder = new cape(0,0,0,0);
 	capeHolder.generate();
 	entity.capePopulation.push(capeHolder);
-	//temporary
-	document.getElementById("power").innerHTML = "Power: " + entity.capePopulation[0].power;
-	document.getElementById("classification").innerHTML = entity.capePopulation[0].ability;
-	document.getElementById("name").innerHTML = entity.capePopulation[0].name;
-	document.getElementById("health").innerHTML = "Survivability: " + entity.capePopulation[0].health;
 	}
-	//end temporary
 }
 function conflictHarvest(){
 	pass = 0;
@@ -104,9 +114,22 @@ function capePowerComparator(a, b){
 function postCapes(){
 	for (var i = entity.capePopulation.length - 1; i >= 0; i--) {
 		c = entity.capePopulation[i];
-		element = document.querySelectorAll("td:nth-of-type(" + (c.abilityIndex + 1) + ") ul");
-		element[0].innerHTML = "<li>Name: "+ c.name +"</li><li>Class: "+ c.ability +"</li><li>Power: "+ c.power +"</li><li>Survivability: "+ c.health +"</li>";
+		element = document.querySelectorAll(".row div:nth-of-type(" + (c.abilityIndex+1) + ")");
+		if(c.posted === false){
+		c.post(element[0]);
+		}
+		//element[0].innerHTML = "<li>Name: "+ c.name +"</li><li>Class: "+ c.ability +"</li><li>Power: "+ c.power +"</li><li>Survivability: "+ c.health +"</li>";
 	}
 }
-
 setInterval(postCapes, 1000);
+
+function killCape(id){
+	for (var i = entity.capePopulation.length - 1; i >= 0; i--) {
+		if (entity.capePopulation[i].phID === id){
+			entity.capePopulation[i].die();
+			delete entity.capePopulation[i];
+		}
+	}
+	entity.capePurge();
+	document.getElementById("capes").innerHTML = entity.capes;	
+}
