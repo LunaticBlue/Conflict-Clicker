2609f6dcbdb82e00f8be1bd1d367c8000e37c488
LunaticBlue <lunaticbleu@gmail.com>
2017-07-14 20:53:30 -0400

    Add files via upload

--- entity.js
+++ entity.js
@@ -1,6 +1,7 @@
+//Total Conflict
+totalConflict = 0
 
-
-//Cape Generator
+//Cape constructor
 var phIDs = 0
 abilityClasses = ["Mover", "Shaker", "Brute", "Breaker", "Master", "Tinker", "Blaster", "Thinker", "Striker", "Changer", "Trump", "Stranger", ]
 abilityPowermods = [1, 3, 5, 0, 5, 0, 5, 1, 7, 3, 0, 1,]  
@@ -31,6 +32,25 @@ function cape(power, health, ability, age){
 		document.getElementById("ph"+ this.phID +"").innerHTML = "";
 	}
 }
+//Endbringer constructor
+var ebIDs = 0
+function endbringer(lethality, range){
+	this.ebID = 0;
+	this.lethality = lethality;
+	this.range = range;
+	this.name = "Typhon";
+	this.posted = false;
+	this.generate = function(){
+		this.lethality = (50+Math.floor(Math.random()*50)+1);
+		this.range = 30;
+		this.ebID = ebIDs;
+		ebIDs += 1;
+	}
+	this.post = function(elem){
+		elem.innerHTML += "<div class = 'endbringerContainer'id = 'eb" + this.ebID + "'><p>Name: "+ this.name +", Lethality: "+ this.lethality +", Range: "+ this.range +" <button onclick='' type='button'>Activate</button></p></div>";
+		this.posted = true;
+	}
+}
 
 //Entity object and system
 var entity = {
@@ -40,12 +60,15 @@ var entity = {
 	shards: 0,
 	capes: 0,
 	capePopulation: [],
+	endbringers: 0,
+	endbringerPopulation: [],
 	dataGather: function(){
 		this.data += 1;
 	},
 	conflictGather: function(j){
 		for (var i = j-1; i >= 0; i--) {
 			this.conflict += 1
+			totalConflict += 1
 			if(this.conflict >= 100){
 				this.conflict -= 100
 				this.data += 1
@@ -56,6 +79,16 @@ var entity = {
 		if(this.data >= 10){
 			this.data -= 10;
 			this.capes += 1;
+			return true;
+		}
+		else{
+			return false;
+		}
+	},
+	endbringerGather: function(){
+		if(this.data >= 1000){
+			this.data -= 1000;
+			this.endbringers +=1;
 			return true;
 		}
 		else{
@@ -77,6 +110,7 @@ var entity = {
 	},
 	
 }
+//HTML-accessible functions
 function addData(){
 	entity.dataGather();
 	document.getElementById("data").innerHTML = entity.data;
@@ -95,6 +129,15 @@ function addCapes(){
 	entity.capePopulation.push(capeHolder);
 	}
 }
+function addEndbringers(){
+	if (entity.endbringerGather() == true){
+		document.getElementById("endbringers").innerHTML = entity.endbringers;
+		document.getElementById("data").innerHTML = entity.data;
+		var endbringerHolder = new endbringer(0,0);
+		endbringerHolder.generate();
+		entity.endbringerPopulation.push(endbringerHolder);
+	}
+}
 function conflictHarvest(){
 	pass = 0;
 	for (var i = entity.capePopulation.length - 1; i >= 0; i--) {
@@ -105,11 +148,6 @@ function conflictHarvest(){
 	document.getElementById("data").innerHTML = entity.data;
 }
 setInterval(conflictHarvest, 1000);
-
-function capePowerComparator(a, b){
-	return parseInt(a.price, 10) - parseInt(b.price, 10);
-}
-
 
 function postCapes(){
 	for (var i = entity.capePopulation.length - 1; i >= 0; i--) {
@@ -122,6 +160,17 @@ function postCapes(){
 }
 setInterval(postCapes, 1000);
 
+function postEndbringers(){
+	for (var i = entity.endbringerPopulation.length - 1; i >= 0; i--) {
+		e = entity.endbringerPopulation[i];
+		element = document.getElementById("tabs-13");
+		if(e.posted === false){
+			e.post(element);
+		}
+	}
+}
+setInterval(postEndbringers, 1000);
+
 function killCape(id){
 	for (var i = entity.capePopulation.length - 1; i >= 0; i--) {
 		if (entity.capePopulation[i].phID === id){
@@ -133,11 +182,15 @@ function killCape(id){
 	document.getElementById("capes").innerHTML = entity.capes;	
 }
 function tabCountUpdate(){
-	lst = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
+	lst = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 	elems = document.querySelectorAll(".tab span");
 	for (var i = entity.capePopulation.length - 1; i >= 0; i--) {
 		lst[entity.capePopulation[i].abilityIndex] += 1;
 		elems[entity.capePopulation[i].abilityIndex].innerHTML = "(" + lst[entity.capePopulation[i].abilityIndex] + ")";
+	}
+	for (var i = entity.endbringerPopulation.length - 1; i >= 0; i--) {
+		lst[12] += 1;
+		elems[12].innerHTML = "(" + lst[12] + ")";
 	}
 }
 setInterval(tabCountUpdate, 1000);
